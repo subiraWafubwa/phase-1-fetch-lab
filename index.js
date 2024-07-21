77d8d59bb4ba118366933433f85ae5ee3f9c83e9
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-21 22:02:17 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => renderBooks(books))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
